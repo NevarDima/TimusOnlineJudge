0a32ff18fb3bb00bfc458fc27372d6fadf6c9b9e
Dima <nevar.dima@gmail.com>
2019-06-06 16:17:21 +0300

    add 1001

--- src/ru/timus/acm/dnevar/ReverseRoot1001/Main.java
+++ src/ru/timus/acm/dnevar/ReverseRoot1001/Main.java
@@ -1,9 +1,26 @@
 package ru.timus.acm.dnevar.ReverseRoot1001;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc=new Scanner(System.in);
+        String line;
+        String[] element = new String[100];
+        int elemPos=0;
+
+        while(!((line=sc.nextLine()).equals("end"))){
+            String[] buffer = line.trim().replaceAll("\\s+"," ").split(" ");
+            System.arraycopy(buffer,0,element,elemPos,buffer.length);
+            elemPos+=buffer.length;
+        }
+
+        for (int i = element.length-1; i >= 0; i--) {
+            if(element[i]!=null){
+                System.out.println(Math.sqrt(Double.parseDouble(element[i])));
+            }
+        }
     }
 }
